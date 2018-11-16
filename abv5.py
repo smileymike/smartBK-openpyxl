5041cc20ae8f7e56eb1429b35c4a571e829a6cce
Smileymike <michael.cunnane@gmail.com>
2018-11-16 21:47:52 +0000

    Added Columun 'Ref' & 'No' on both worksheets

--- abv5.py
+++ abv5.py
@@ -16,6 +16,9 @@ DEST_FILENAME = '/home/anonymous/cashbookTaxYr2018-2019.xlsx'
 COMPANY_NAME = '<Company Name> Company'
 TAX_YEAR_ENDED = 'Year Ended 31-03-2019 (Start 1st April 2018)'
 
+# ABBV of "Starting Point For Catagories"
+SPFC = 10
+
 ROW_SPACE_BEFORE_TOTAL = 1
 COLUMN_SPACE_BEFORE_TOTAL = 1
 SPACE_AND_CHECK_COL = 2
@@ -54,15 +57,16 @@ except:
 	receipts['A1'] = COMPANY_NAME
 	receipts['A2'] = TAX_YEAR_ENDED
 	receipts['A3'] = 'Cashbook Receipts'
-	receipts['C4'] = 'Gross Amount'
-	receipts['A5'] = 'Date'
-	receipts['B5'] = 'Description'
-	receipts['C5'] = 'Bank'
-	receipts['D5'] = 'Cash'
-	receipts['E5'] = 'Other'
-	receipts['F5'] = 'VAT'
-	receipts['G5'] = 'Net'
-	receipts['H5'] = 'Analysis'
+	receipts['D4'] = 'Gross Amount'
+	receipts['A5'] = 'Ref'		
+	receipts['B5'] = 'Date'
+	receipts['C5'] = 'Description'
+	receipts['D5'] = 'Bank'
+	receipts['E5'] = 'Cash'
+	receipts['F5'] = 'Other'
+	receipts['G5'] = 'VAT'
+	receipts['H5'] = 'Net'
+	receipts['I5'] = 'Analysis'
 
 	# Bolds
 	receipts['A1'].font = FONT_BOLD
@@ -71,25 +75,29 @@ except:
 
 
 	# Merge cells in Gross
-	receipts.merge_cells('C4:E4')
+	receipts.merge_cells('D4:G4')
 	receipts['C4'].alignment = CENTER
 
-	for col in receipts.iter_cols(min_row=4, max_col=8, max_row=5):
+	for col in receipts.iter_cols(min_row=4, max_col=9, max_row=5):
 		for cell in col:
 			cell.font = FONT_BOLD
 			cell.alignment = CENTER
 
 	receipts.column_dimensions['A'].width = 11
-	receipts.column_dimensions['B'].width = 32
-	receipts.column_dimensions['C'].width = FIVE_FIGURE_COLUMN
+	receipts.column_dimensions['B'].width = 11
+	receipts.column_dimensions['C'].width = 32
 	receipts.column_dimensions['D'].width = FIVE_FIGURE_COLUMN
 	receipts.column_dimensions['E'].width = FIVE_FIGURE_COLUMN
 	receipts.column_dimensions['F'].width = FIVE_FIGURE_COLUMN
-	receipts.column_dimensions['G'].width = SIX_FIGURE_COLUMN
+	receipts.column_dimensions['G'].width = FIVE_FIGURE_COLUMN
+	receipts.column_dimensions['H'].width = SIX_FIGURE_COLUMN
+	receipts.column_dimensions['I'].width = SIX_FIGURE_COLUMN
+	receipts.column_dimensions['J'].width = SIX_FIGURE_COLUMN
 
 	payments = cashbook.copy_worksheet(receipts)
 	payments.title = 'Cashbook Payments'
 	payments['A3'] = 'Cashbook Payments'
+	payments['A5'] = 'No'
 	payments.sheet_view.zoomScale = 75
 
 	receipts_catagories = []
@@ -100,11 +108,13 @@ except:
 		for row in reader:
 			receipts_catagories.append(''.join(row))
 
-	for col in range(10,10+len(receipts_catagories)):
-		_ = receipts.cell(column=col, row=5, value="{0}".format(receipts_catagories[col-10]))
+	SPFC = 11
+
+	for col in range(SPFC,SPFC+len(receipts_catagories)):
+		_ = receipts.cell(column=col, row=5, value="{0}".format(receipts_catagories[col-SPFC]))
 		receipts.column_dimensions[get_column_letter(col)].width = FIVE_FIGURE_COLUMN
 
-	receipts_check_col = 10+len(receipts_catagories)+COLUMN_SPACE_BEFORE_TOTAL
+	receipts_check_col = SPFC+len(receipts_catagories)+COLUMN_SPACE_BEFORE_TOTAL
 	receipts.cell(column=receipts_check_col, row=5, value="Check")
 	receipts.column_dimensions[get_column_letter(receipts_check_col)].width = SIX_FIGURE_COLUMN
 
@@ -121,14 +131,14 @@ except:
 		for row in reader:
 			payments_catagories.append(''.join(row))
 
-	for col in range(10,10+len(payments_catagories)):
-		_ = payments.cell(column=col, row=5, value="{0}".format(payments_catagories[col-10]))
+	for col in range(SPFC,SPFC+len(payments_catagories)):
+		_ = payments.cell(column=col, row=5, value="{0}".format(payments_catagories[col-SPFC]))
 
-	payments_check_col = 10+len(payments_catagories)+COLUMN_SPACE_BEFORE_TOTAL
+	payments_check_col = SPFC+len(payments_catagories)+COLUMN_SPACE_BEFORE_TOTAL
 	payments.cell(column=payments_check_col, row=5, value="Check")
 	payments.column_dimensions[get_column_letter(payments_check_col)].width = SIX_FIGURE_COLUMN
 
-	for col in payments.iter_cols(min_row=5, min_col=10, max_col=payments.max_column, max_row=5):
+	for col in payments.iter_cols(min_row=5, min_col=SPFC, max_col=payments.max_column, max_row=5):
 		for cell in col:
 			cell.font = FONT_BOLD
 			cell.alignment = CENTER_AND_WRAP_TEXT
@@ -177,67 +187,69 @@ except:
 
 				row[2] = float(row[2])
 
-				transactions.append(row)	
+				prefix_row = [' '] + row
+
+				transactions.append(prefix_row)	
 
 	for each_transaction in reversed(transactions):
-		if each_transaction[2] > 0.00:
+		if each_transaction[3] > 0.00:
 			receipt_transaction_tally += 1
 			receipts.append(each_transaction)
-		elif each_transaction[2] <= 0.00:
+		elif each_transaction[3] <= 0.00:
 			# remove negative symbol
-			each_transaction[2] = abs(each_transaction[2])
+			each_transaction[3] = abs(each_transaction[3])
 			payment_transaction_tally += 1
 			payments.append(each_transaction)
 
 	# Date Column
-	for row in receipts.iter_rows(min_row=6, max_col=1,max_row=receipts.max_row):
+	for row in receipts.iter_rows(min_row=6, max_col=2,max_row=receipts.max_row):
 		for cell in row:
 			cell.number_format = "DD/MM/YYYY"
-			cell.alignment = Alignment(horizontal='left')
+			cell.alignment = Alignment(horizontal='center')
 
-	for row in payments.iter_rows(min_row=6, max_col=1,max_row=payments.max_row):
+	for row in payments.iter_rows(min_row=6, max_col=2,max_row=payments.max_row):
 		for cell in row:
 			cell.number_format = "DD/MM/YYYY"
-			cell.alignment = Alignment(horizontal='left')
+			cell.alignment = Alignment('center')
 
 	# "Net" Amount Column
 	for row in range(6,receipt_transaction_tally+6):
-		_ = receipts.cell(column=7, row=row, value="=SUM(C{0}:E{0})-F{0}".format(row))
+		_ = receipts.cell(column=8, row=row, value="=SUM(C{0}:E{0})-F{0}".format(row))
 
 	for row in range(6,payment_transaction_tally+6):
-		_ = payments.cell(column=7, row=row, value="=SUM(C{0}:E{0})-F{0}".format(row))	
+		_ = payments.cell(column=8, row=row, value="=SUM(C{0}:E{0})-F{0}".format(row))	
 
 	# Gross Amount Columns - Bank, Cash, Other, VAT
-	for col in range(3, 8):
+	for col in range(4, 9):
 		_ = receipts.cell(column=col, row=6+receipt_transaction_tally+ROW_SPACE_BEFORE_TOTAL, value="=SUM({0}6:{0}{1})".format(get_column_letter(col),5+receipt_transaction_tally))
 
-	for col in range(3, 8):
+	for col in range(4, 9):
 		_ = payments.cell(column=col, row=6+payment_transaction_tally+ROW_SPACE_BEFORE_TOTAL, value="=SUM({0}6:{0}{1})".format(get_column_letter(col),5+payment_transaction_tally))
 
 	# catagorisation formula for Gross Amount
 	for row in range(6,6+receipt_transaction_tally):
-		for col in range(10,receipts.max_column-1):
-			_ = receipts.cell(column=col, row=row, value='=IF({0}$5=${2}{1},${3}{1},"")'.format(get_column_letter(col),row,'H','G'))
+		for col in range(SPFC,receipts.max_column-1):
+			_ = receipts.cell(column=col, row=row, value='=IF({0}$5=${2}{1},${3}{1},"")'.format(get_column_letter(col),row,'I','H'))
 
 	for row in range(6,6+payment_transaction_tally):
-		for col in range(10,payments.max_column-1):
-			_ = payments.cell(column=col, row=row, value='=IF({0}$5=${2}{1},${3}{1},"")'.format(get_column_letter(col),row,'H','G'))
+		for col in range(SPFC,payments.max_column-1):
+			_ = payments.cell(column=col, row=row, value='=IF({0}$5=${2}{1},${3}{1},"")'.format(get_column_letter(col),row,'I','H'))
 
 	# Total amounts under each catagorised items e.g. Sales, Drawings etc.
 	for row in range(6,6+receipt_transaction_tally):
-		for col in range(10,receipts.max_column-1):
+		for col in range(SPFC,receipts.max_column-1):
 			_ = receipts.cell(column=col, row=6+receipt_transaction_tally+ROW_SPACE_BEFORE_TOTAL, value="=SUM({0}6:{0}{1})".format(get_column_letter(col),5+receipt_transaction_tally))
 
 	for row in range(6,6+payment_transaction_tally):
-		for col in range(10,payments.max_column-1):
+		for col in range(SPFC,payments.max_column-1):
 			_ = payments.cell(column=col, row=6+payment_transaction_tally+ROW_SPACE_BEFORE_TOTAL, value="=SUM({0}6:{0}{1})".format(get_column_letter(col),5+payment_transaction_tally))
 
 	# Calculation for check or unchecked transactions
 	for row in range(6,receipt_transaction_tally+6):
-		_ = receipts.cell(column=receipts.max_column, row=row, value="=SUM({1}{0}:J{0})-G{0}".format(row,get_column_letter(receipts.max_column-2)))
+		_ = receipts.cell(column=receipts.max_column, row=row, value="=SUM({1}{0}:K{0})-H{0}".format(row,get_column_letter(receipts.max_column-2)))
 
 	for row in range(6,payment_transaction_tally+6):
-		_ = payments.cell(column=payments.max_column, row=row, value="=SUM({1}{0}:J{0})-G{0}".format(row,get_column_letter(payments.max_column-2)))
+		_ = payments.cell(column=payments.max_column, row=row, value="=SUM({1}{0}:K{0})-H{0}".format(row,get_column_letter(payments.max_column-2)))
 
 	# Total amount in check or unchecked transactions
 	_ = receipts.cell(column=receipts.max_column, row=6+receipt_transaction_tally+ROW_SPACE_BEFORE_TOTAL, value="=SUM({0}6:{0}{1})".format(get_column_letter(receipts.max_column),5+receipt_transaction_tally,'H','G'))
@@ -267,7 +279,7 @@ except:
 				cell.font = FONT_BOLD
 				cell.border = TOTAL_BORDER_LINES
 
-	for col in receipts.iter_cols(min_row=5, min_col=10, max_col=receipts.max_column-SPACE_AND_CHECK_COL, max_row=5):
+	for col in receipts.iter_cols(min_row=5, min_col=SPFC, max_col=receipts.max_column-SPACE_AND_CHECK_COL, max_row=5):
 		for cell in col:
 			heading = cell.value
 			longest_width = FIVE_FIGURE_COLUMN
@@ -276,7 +288,7 @@ except:
 					longest_width = len(word) + 3
 				receipts.column_dimensions[cell.column].width =  longest_width + 1
 
-	for col in payments.iter_cols(min_row=5, min_col=10, max_col=payments.max_column-SPACE_AND_CHECK_COL, max_row=5):
+	for col in payments.iter_cols(min_row=5, min_col=SPFC, max_col=payments.max_column-SPACE_AND_CHECK_COL, max_row=5):
 		for cell in col:
 			heading = cell.value
 			longest_width = FIVE_FIGURE_COLUMN
